23175fdbe945c2727736a1540bb4338adb6515f8
Beatrice-cyber <93314184+Beatrice-cyber@users.noreply.github.com>
2026-09-15 19:09:01 +0100

    made snapping for all 3 different types of conveyors

--- Assets/Scripts/ConveyorPlacer.cs
+++ Assets/Scripts/ConveyorPlacer.cs
@@ -4,38 +4,106 @@ using System.Collections.Generic;
 //this is a class for managing conveyors
 public class ConveyorPlacer : MonoBehaviour
 {
-    [SerializeField] private GameObject conveyorPrefab;
+	[SerializeField] private GameObject conveyorPrefab;
 	[SerializeField] private LayerMask groundLayer;
-    private GameObject previewConveyor;
+	[SerializeField] private float SnapThreshold = 1.0f;
+	private GameObject previewConveyor;
+	private GameObject selectedPrefab;
 	private List<Conveyor> placedConveyors = new List<Conveyor>();
 
-    [SerializeField] private GameObject shortConveyorPrefab;
-    [SerializeField] private GameObject inclineConveyorPrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        previewConveyor = Instantiate(conveyorPrefab);
+	[SerializeField] private GameObject shortConveyorPrefab;
+	[SerializeField] private GameObject inclineConveyorPrefab;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		//default is long conveyor but this can be changed to other types of conveyor
+		selectedPrefab = conveyorPrefab;
+		previewConveyor = Instantiate(selectedPrefab);
+	}
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+	// Update is called once per frame
+	void Update()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
 		// starting pt, store result, how long can ray hit, ONLY hit layers in groundlayer
-        if (Physics.Raycast(ray, out hit , Mathf.Infinity, groundLayer))
-        {
-            previewConveyor.transform.position = hit.point;
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+		{
+			if (Input.GetKeyDown(KeyCode.Alpha1)) { SelectConveyor(conveyorPrefab); }
+			if (Input.GetKeyDown(KeyCode.Alpha2)) { SelectConveyor(shortConveyorPrefab); }
+			if (Input.GetKeyDown(KeyCode.Alpha3)) { SelectConveyor(inclineConveyorPrefab); }
+			previewConveyor.transform.position = hit.point;
+			if (placedConveyors.Count > 0)
+			{
+				SnapConveyors();
+			}
 			// if (Mouse.current.leftButton.wasPressedThisFrame)
-			if(Input.GetMouseButtonDown(0))
+			if (Input.GetMouseButtonDown(0))
 			{
 				//instantiate a conveyor(of type gameobject) at the place of hit 
-				GameObject conveyorObj=Instantiate(conveyorPrefab, previewConveyor.transform.position, previewConveyor.transform.rotation); 
+				GameObject conveyorObj = Instantiate(selectedPrefab, previewConveyor.transform.position, previewConveyor.transform.rotation);
 				Conveyor placedConveyor = conveyorObj.GetComponent<Conveyor>();
 				placedConveyors.Add(placedConveyor);
 			}
-        }
+		}
 
-    }
+	}
+	private void SelectConveyor(GameObject prefab)
+	{
+		selectedPrefab = prefab;
+		// old preview is the wrong type
+		Destroy(previewConveyor);
+		// create preview of newly selected type
+		previewConveyor = Instantiate(selectedPrefab);
+	}
+
+	private void SnapConveyors()
+	{
+		// takes a current moouse position and go through the list to see if distance is < threshold 
+		// to decide whether snap or not 
+		//need access to previewconveyor.endpt
+		Conveyor preview = previewConveyor.GetComponent<Conveyor>();
+		Conveyor nearest = NearestConveyor(preview);
+		if (nearest == null) { return; }
+		float distance = Vector3.Distance(preview.StartPoint.position, nearest.EndPoint.position);
+		//&& distance < SnapThreshold
+		if (distance <= SnapThreshold)
+		{
+			Debug.Log("SNAPPING! Distance = " + distance);
+			// snap 2 conveyors together, move preview by offset 
+			Vector3 offset = nearest.EndPoint.position - preview.StartPoint.position;
+			Debug.Log("Offset = " + offset);
+			previewConveyor.transform.position += offset;
+			Debug.Log("After snap distance = " + Vector3.Distance(
+			preview.StartPoint.position,
+			nearest.EndPoint.position));
+		}
+
+
+	}
+
+	// returns a conveyor of minimum distance  
+	private Conveyor NearestConveyor(Conveyor preview)
+	{
+		float minDist = Mathf.Infinity;
+		Conveyor minConveyor = null;
+		for (int i = 0; i < placedConveyors.Count; i++)
+		{
+			// there are 2 directions for distance
+			if (placedConveyors[i] is null) { Debug.Log("PlacedConveys[i] is null"); }
+			if (preview is null)
+			{
+				Debug.Log("preview");
+			}
+
+			float dist1 = Vector3.Distance(placedConveyors[i].EndPoint.position, preview.StartPoint.position);
+			// dist2 = Vector3.Distance(placedConveyors[i].StartPoint.position, preview.EndPoint.position);
+			if (minDist >= dist1)
+			{
+				minConveyor = placedConveyors[i];
+				minDist = dist1;
+			}
+		}
+		return minConveyor;
+	}
 }
